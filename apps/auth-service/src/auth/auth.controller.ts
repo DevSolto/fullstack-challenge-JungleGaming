2432d239b8ef0ba26a5d3b99a71e121814491bdf
santiago <santiagosiqueirasouto@gmail.com>
2025-09-27 17:39:00 -0300

    base

--- apps/auth-service/src/auth/auth.controller.ts
+++ apps/auth-service/src/auth/auth.controller.ts
@@ -1,14 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/auth.dto';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
-  @MessagePattern('login')
-  create(@Payload() loginDto: LoginDto) {
+  @Post('login')
+  async create(@Payload() loginDto: LoginDto) {
+    const password = await loginDto.password;
     return this.authService.create(loginDto);
   }
 }
--- apps/auth-service/src/auth/auth.service.ts
+++ apps/auth-service/src/auth/auth.service.ts
@@ -1,26 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { LoginDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  create(loginDto: LoginDto) {
+    return 'login: ' + JSON.stringify(loginDto);
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
 }
